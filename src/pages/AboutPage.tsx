84bc035b144a9699ab556fad209727df7051de3c
AshishNith <ranjan.ashish9992@gmail.com>
2025-09-15 12:11:28 +0530

    uupdate

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -302,6 +302,8 @@ const AboutPage: React.FC = () => {
           <div className="absolute -bottom-24 -right-24 w-96 h-96 bg-[var(--accent-secondary)] opacity-10 rounded-full blur-3xl"></div>
         </div>
       </section>
+
+      
     </div>
   );
 };
