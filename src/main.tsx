b907c19882efa5540154a05a1768ec3f7db98d91
AshishNith <ranjan.ashish9992@gmail.com>
2025-09-27 16:24:09 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,32 +4,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Import Lenis for smooth scrolling
-import Lenis from '@studio-freight/lenis';
-
-// Initialize Lenis smooth scrolling
-function useLenis() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      lerp: 0.03,
-      smoothWheel: true,
-      orientation: 'vertical',
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-}
 
 function AppWithLenis() {
-  useLenis();
+  // useLenis();
   return <App />;
 }
 
